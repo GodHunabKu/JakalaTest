a281e161bfedf67f9f7a30a06fdf759457ef5f72
GodHunabKu <152228420+GodHunabKu@users.noreply.github.com>
2023-11-28 14:57:42 +0100

    Add files via upload

---
+++ javatest v/src/main/java/com/javatest/service/CustomerService.java
@@ -0,0 +1,34 @@
+package com.javatest.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.javatest.entity.Customer;
+import com.javatest.repository.CustomerRepository;
+
+@Service
+public class CustomerService {
+
+    private final CustomerRepository customerRepository;
+
+    @Autowired
+    public CustomerService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    public List<Customer> getAllCustomers() {
+        return customerRepository.findAll();
+    }
+
+    public Optional<Customer> getCustomerById(Long customerId) {
+        return customerRepository.findById(customerId);
+    }
+
+    public Customer createCustomer(Customer customer) {
+        return customerRepository.save(customer);
+    }
+
+}
